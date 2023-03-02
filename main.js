301ddf6dfa97867b2f0e204a01d8b00472df98c8
Dalton Skerlec <dskerlec@gmail.com>
2023-03-01 22:29:14 -0500

    update js to update html with temps for daily and hourly

--- main.js
+++ main.js
@@ -1,31 +1,92 @@
 'use strict';
- const form = document.querySelector('form');
- const cityInput = document.querySelector('#city');
- const stateInput = document.querySelector('#state');
+//TODO: allow user to select a city state and geocode it to return long/lat
+//possibly use a dropdown and store long/lat? or use another api call
+ //const cityInput = document.querySelector('#city');
+ //const stateInput = document.querySelector('#state');
  //const latInput = document.querySelector('#latitude');
- const latInput = '41.50';
  //const longInput = document.querySelector('#longitude');
+ const latInput = '41.50';
  const longInput = '-81.70';
  const todayDiv = document.querySelector('#dailyOne');
  const tomorrowDiv = document.querySelector('#dailyTwo');
  const overmorrowDiv = document.querySelector('#dailyThree');
  const nowDiv = document.querySelector('#hourlyOne');
  const nextHoursDiv = document.querySelector('#hourlyTwo');
- const afterHoursDiv = document.querySelector('#hourlyThree');
-form.addEventListener('submit', getWeather(latInput, longInput))
+ const laterHoursDiv = document.querySelector('#hourlyThree');
+ let date = new Date();
+ const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+ let day = getCurrentDate(date);
+ let hour = getCurrentHour(date);
+ getWeather(latInput, longInput)
 
-  function getWeather(lat, long) {
+
+  function getWeather(lat, long) 
+  {
      // const city = cityInput.value;
     // const apiCityToLongLatUrl = 'https://nominatim.openstreetmap.org/search?q=${cityInput},${stateInput}';
     const apiWeatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&temperature_unit=fahrenheit&currenttemperature=true&timezone=EST&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m`;
-    console.log(apiWeatherUrl);
-    console.log("lat" + lat);
-    console.log("long" + long);
     fetch(apiWeatherUrl)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        //forecastDiv.innerHTML = `Current Temperature: ${currentTempF}°F<br>In one hour it will be: ${nextTempF}°F`;
+        let result = data;
+
+        let todayHigh = getTemp(result, "daily", 0, "max");
+        let todayLow = getTemp(result, "daily", 0, "min");
+        let tomorrowHigh = getTemp(result, "daily", 1, "max");
+        let tomorrowLow = getTemp(result, "daily", 1, "min");
+        let overmorrowHigh = getTemp(result, "daily", 2, "max");
+        let overmorrowLow = getTemp(result, "daily", 1, "min");
+
+        //TODO:get dates and replace "today tomorrow and overmorrow with actual dates"
+        todayDiv.innerHTML = `${day}:<br>High Temperature: ${todayHigh}°F<br>Low Temperature: ${todayLow}°F`
+        tomorrowDiv.innerHTML = `Tomorrow's Temperatures:<br>High Temperature: ${tomorrowHigh}°F<br>Low Temperature: ${tomorrowLow}°F`
+        overmorrowDiv.innerHTML = `Overmorrow's Temperatures:<br>High Temperature: ${overmorrowHigh}°F<br>Low Temperature: ${overmorrowLow}°F`
+
+        //TODO: get current time
+        let nowTemp = getTemp(result, "hourly", hour);
+        //let nowPrecip
+        let nextTemp = getTemp(result, "hourly", (hour+3));
+        //let nextPrecip
+        let laterTemp = getTemp(result, "hourly", (hour+6));
+        //let laterPrecip
+
+        //TODO: update with precip codes, 
+        //need a constant to list out the codes
+        //possibly correlate each code with an image???
+        nowDiv.innerHTML = `Now:<br>Current Temperature: ${nowTemp}°F<br>Current Precipitation:`
+        nextHoursDiv.innerHTML = `in 3 Hours:<br>Current Temperature: ${nextTemp}°F<br>Current Precipitation:`
+        laterHoursDiv.innerHTML = `in 6 Hours:<br>Current Temperature: ${laterTemp}°F<br>Current Precipitation:`
+
       })
       .catch(error => console.error(error));
+  }
+
+  //break out into getDailyTemp and getHourlyTemp??
+  function getTemp(data, forecastType, timeInterval, dailyType)
+  {
+    switch (forecastType) 
+    {
+      case "daily":
+        if (dailyType === "max")
+          return data.daily.temperature_2m_max[timeInterval];
+        else 
+          return data.daily.temperature_2m_min[timeInterval];
+      case "hourly":
+        return data.hourly.temperature_2m[timeInterval];
+      default:
+        //error
+    }
+  }
+  
+  function getCurrentHour(date)
+  {
+      return date.getHours();
+  }
+
+  function getCurrentDate(date)
+  {
+      let month = months[date.getMonth()];
+      let day = date.getDate();
+      let year = date.getFullYear();
+      return `${month} ${day} , ${year}`
   }
