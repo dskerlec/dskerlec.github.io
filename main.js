2f9c9e7a00cb56773c2149ea135d6079c149eda0
bhughes9 <bradhughes9957@gmail.com>
2023-03-03 19:15:25 -0500

    add precipitation HOURLY function

--- main.js
+++ main.js
@@ -20,7 +20,8 @@
     fetch(apiWeatherUrl)
       .then(response => response.json())
       .then(data => {
-
+        //console log to view the variable in console for working with it. Remove this for production
+        console.log(data);
         let today = getDay(data, 0);
         let todayHigh = getTemp(data, "daily", 0, "max");
         let todayLow = getTemp(data, "daily", 0, "min");
@@ -30,6 +31,10 @@
         let overmorrow = getDay(data, 2);
         let overmorrowHigh = getTemp(data, "daily", 2, "max");
         let overmorrowLow = getTemp(data, "daily", 1, "min");
+        //precipitation variables
+        let nowPrecipitation = getPrecipitation(data, hour);
+        let nextPrecipitation = getPrecipitation(data, (hour+3));
+        let laterPrecipitation = getPrecipitation(data, (hour+6));
 
         //TODO:clean up actual dates
         todayDiv.innerHTML = `${today}:<br>High Temperature: ${todayHigh}°F<br>Low Temperature: ${todayLow}°F`
@@ -45,18 +50,23 @@
         //let nextPrecip
         let later = getHour(data, (hour+6));
         let laterTemp = getTemp(data, "hourly", (hour+6));
-        //let laterPrecip
+        
 
         //TODO: update with precip codes,
         //need a constant to list out the codes
         //possibly correlate each code with an image???
-        nowDiv.innerHTML = `At ${now} :<br>Current Temperature: ${nowTemp}°F<br>Current Precipitation:`
-        nextHoursDiv.innerHTML = `At ${next} :<br>Current Temperature: ${nextTemp}°F<br>Current Precipitation:`
-        laterHoursDiv.innerHTML = `At ${later} :<br>Current Temperature: ${laterTemp}°F<br>Current Precipitation:`
+        nowDiv.innerHTML = `At ${now} :<br>Current Temperature: ${nowTemp}°F<br>Current Precipitation:${nowPrecipitation}`
+        nextHoursDiv.innerHTML = `At ${next} :<br>Current Temperature: ${nextTemp}°F<br>Current Precipitation:${nextPrecipitation}`
+        laterHoursDiv.innerHTML = `At ${later} :<br>Current Temperature: ${laterTemp}°F<br>Current Precipitation:${laterPrecipitation}`
 
       })
       .catch(error => console.error(error));
   }
+
+//get daily precipitation
+function getPrecipitation(data, hour) {
+  return data.hourly.precipitation_probability[hour];
+}
 
   //break out into getDailyTemp and getHourlyTemp??
   function getTemp(data, forecastType, timeInterval, dailyType)
@@ -89,3 +99,5 @@
   {
     return data.hourly.time[hourIndex];
   }
+
+  
